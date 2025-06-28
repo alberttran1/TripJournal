46aa5d912519421857f255bb13ced306a7f73fb7
Albert Tran <albertq.tran1@gmail.com>
2025-06-28 02:22:35 -0400

    env var test

--- client/src/Api/http.tsx
+++ client/src/Api/http.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { auth } from "../../firebase"; // your firebase.ts
 
+console.log(import.meta.env.VITE_API_URL)
+
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api', // adjust as needed
   withCredentials: true, // if using cookies/session
