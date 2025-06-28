7acca60a3d9436c4f9fd642b629fc44dab9d561f
Albert Tran <albertq.tran1@gmail.com>
2025-06-28 02:25:33 -0400

    removed log

--- client/src/Api/http.tsx
+++ client/src/Api/http.tsx
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { auth } from "../../firebase"; // your firebase.ts
 
-console.log(import.meta.env.VITE_API_URL)
-
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api', // adjust as needed
   withCredentials: true, // if using cookies/session
